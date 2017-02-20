6e376723c0cc2d82642739854c21bc3c7fe6d3ca
mathieui <mathieui@mathieui.net>
2017-02-20 22:12:24 +0100

    Add base models (sub/msg/user)

---
+++ unha2/model/base.py
@@ -0,0 +1,66 @@
+import enum
+from .. common import ts
+
+class User:
+    __slots__ = ['id', 'name']
+    def __init__(self, obj):
+        self.id = obj['_id']
+        self.name = obj['username']
+
+    def __eq__(self, other):
+        return self.id == other.id and self.name == other.name
+
+class RoomType(enum.Enum):
+    CHAT = 'c'
+    PRIVATE = 'p'
+    DIRECT = 'd'
+
+class Subscription:
+    def __init__(self, obj):
+        self.type = RoomType(obj['t'])
+        self.creation_time = ts(obj['ts'])
+        self.last_seen_message = ts(obj['ls'])
+        self.room_name = obj['name']
+        self.room_id = obj['rid']
+        self.user = User(obj['u'])
+        self.open = obj['open']
+        self.alert = obj['alert']
+        self.roles = obj.get('roles', [])
+        self.unread = obj['unread']
+        self.last_update = ts(obj['_updatedAt'])
+        self.sub_id = obj[ '_id']
+
+class Message:
+    def __init__(self, msg):
+        self.id = msg['_id']
+        self.room_id = msg['rid']
+        self.text = msg['msg']
+        self.sent_at = ts(msg['ts'])
+        self.user = User(msg['u'])
+        self.received_at = ts(msg['_updatedAt'])
+
+        if msg.get('editedAt'):
+            self.edited_at = ts(msg['editedAt'])
+            self.edited_by = User(msg['editedBy'])
+        else:
+            self.edited_at = None
+            self.edited_by = ''
+
+        self.urls = msg.get('urls', [])
+        self.attachments = msg.get('attachments', [])
+        self.alias = msg.get('alias', self.user.name)
+        self.avatar_url = msg.get('avatar', '')
+        self.groupable = msg.get('groupable', False)
+        self.parseUrls = msg.get('parseUrls', False)
+
+class UrlMeta:
+    def __init__(self, obj):
+        self.url = obj['url']
+        self.meta = obj['meta']
+        self.headers = obj['headers']
+        self.parsed_url = obj['parsedUrl']
+
+class Attachment:
+    def __init__(self, obj):
+        self.image_url = obj['image_url']
+        self.color = obj.get('color')
--- unha2/model/room_events.py
+++ unha2/model/room_events.py
@@ -1,107 +1,104 @@
-import enum
-from datetime import datetime
-
-class RoomType(enum.Enum):
-    CHAT = 'c'
-    PRIVATE = 'p'
-    DIRECT = 'd'
+from . base import User, RoomType
+from .. common import ts
 
 class RoomEvent:
     def __init__(self, msg, roomargs):
         self.id = msg['_id']
         self.room = msg['rid']
-        self.time = datetime.fromtimestamp(msg['ts']['$date'] / 1000)
+        self.time = ts(msg['ts'])
         self.room_type = RoomType(roomargs['roomType'])
 
 class RoomJoined(RoomEvent):
-    __slots__ = ['id', 'room', 'userid', 'username', 'time', 'room_type']
+    __slots__ = ['id', 'room', 'user', 'time', 'room_type']
     def __init__(self, msg, roomargs):
         super().__init__(msg, roomargs)
-        self.userid = msg['u']['_id']
-        self.username = msg['u']['username']
+        self.user = User(msg['u'])
     def __repr__(self):
-        return '%s joined the room %s' % (self.username, self.room)
+        return '%s joined the room %s' % (self.user, self.room)
 
 class RoomLeft(RoomEvent):
-    __slots__ = ['id', 'room', 'userid', 'username', 'time', 'room_type']
+    __slots__ = ['id', 'room', 'user', 'time', 'room_type']
     def __init__(self, msg, roomargs):
         super().__init__(msg, roomargs)
-        self.userid = msg['u']['_id']
-        self.username = msg['u']['username']
+        self.user = User(msg['u'])
     def __repr__(self):
-        return '%s left the room %s' % (self.username, self.room)
+        return '%s left the room %s' % (self.user, self.room)
 
 class RoomAdded(RoomEvent):
-    __slots__ = ['id', 'room', 'time', 'user', 'actor', 'actorid', 'room_type']
+    __slots__ = ['id', 'room', 'time', 'user', 'actor', 'room_type']
     def __init__(self, msg, roomargs):
         super().__init__(msg, roomargs)
         self.user = msg['msg']
-        self.actorid= msg['u']['_id']
-        self.actor = msg['u']['username']
+        self.actor = User(msg['u'])
     def __repr__(self):
         return '%s added %s to the room %s' % (self.actor, self.user, self.room)
 
 class RoomRemoved(RoomEvent):
-    __slots__ = ['id', 'room', 'time', 'user', 'actor', 'actorid', 'room_type']
+    __slots__ = ['id', 'room', 'time', 'user', 'actor', 'room_type']
     def __init__(self, msg, roomargs):
         super().__init__(msg, roomargs)
         self.user = msg['msg']
-        self.actorid= msg['u']['_id']
-        self.actor = msg['u']['username']
+        self.actor = User(msg['u'])
     def __repr__(self):
         return '%s removed %s from the room %s' % (self.actor, self.user, self.room)
 
 class RoomRoleAdded(RoomEvent):
-    __slots__ = ['id', 'room', 'role', 'time', 'user', 'actor', 'actorid', 'room_type']
+    __slots__ = ['id', 'room', 'role', 'time', 'user', 'actor', 'room_type']
     def __init__(self, msg, roomargs):
         super().__init__(msg, roomargs)
         self.user = msg['msg']
         self.role = msg['role']
-        self.actorid= msg['u']['_id']
-        self.actor = msg['u']['username']
+        self.actor = User(msg['u'])
     def __repr__(self):
         return '%s gave %s the %s role in %s' % (self.actor, self.user, self.role, self.room)
 
 class RoomRoleRemoved(RoomEvent):
-    __slots__ = ['id', 'room', 'role', 'time', 'user', 'actor', 'actorid', 'room_type']
+    __slots__ = ['id', 'room', 'role', 'time', 'user', 'actor', 'room_type']
     def __init__(self, msg, roomargs):
         super().__init__(msg, roomargs)
         self.user = msg['msg']
         self.role = msg['role']
-        self.actorid= msg['u']['_id']
-        self.actor = msg['u']['username']
+        self.actor = User(msg['u'])
     def __repr__(self):
         return '%s removed the %s role from %s in %s' % (self.actor, self.role, self.user, self.room)
 
 class RoomMuted(RoomEvent):
-    __slots__ = ['id', 'room', 'time', 'user', 'actor', 'actorid', 'room_type']
+    __slots__ = ['id', 'room', 'time', 'user', 'actor', 'room_type']
     def __init__(self, msg, roomargs):
         super().__init__(msg, roomargs)
         self.user = msg['msg']
-        self.actorid= msg['u']['_id']
-        self.actor = msg['u']['username']
+        self.actor = User(msg['u'])
     def __repr__(self):
         return '%s muted %s in %s' % (self.actor, self.user, self.room)
 
 class RoomUnmuted(RoomEvent):
-    __slots__ = ['id', 'room', 'time', 'user', 'actor', 'actorid', 'room_type']
+    __slots__ = ['id', 'room', 'time', 'user', 'actor', 'room_type']
     def __init__(self, msg, roomargs):
         super().__init__(msg, roomargs)
         self.user = msg['msg']
-        self.actorid= msg['u']['_id']
-        self.actor = msg['u']['username']
+        self.actor = User(msg['u'])
     def __repr__(self):
         return '%s unmuted %s in %s' % (self.actor, self.user, self.room)
 
 class RoomMessage(RoomEvent):
-    __slots__ = ['id', 'room', 'time', 'text', 'username', 'urls', 'reactions', 'userid', 'room_type']
+    __slots__ = ['id', 'room', 'time', 'text', 'user', 'urls', 'reactions', 'room_type']
     def __init__(self, msg, roomargs):
         super().__init__(msg, roomargs)
-        self.username = msg['u']['username']
-        self.userid = msg['u']['_id']
+        self.user = User(msg['u'])
         self.text = msg['msg']
         self.urls = msg.get('urls', [])
         self.reactions = msg.get('reactions', {})
     def __repr__(self):
-        return '%s sent on %s: %s' % (self.username, self.room, self.text)
+        return '%s sent on %s: %s' % (self.user, self.room, self.text)
 
+T_TO_OBJ = {
+    'uj': RoomJoined,
+    'ul': RoomLeft,
+    'au': RoomAdded,
+    'ru': RoomRemoved,
+    'subscription-role-added': RoomRoleAdded,
+    'subscription-role-removed': RoomRoleRemoved,
+    'user-muted': RoomMuted,
+    'user-unmuted': RoomUnmuted,
+    'room-changed-topic': lambda *args: None
+}
