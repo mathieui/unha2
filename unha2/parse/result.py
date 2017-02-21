403854acbbcafb9c2039aa1b41b798de04b8601e
mathieui <mathieui@mathieui.net>
2017-02-21 23:55:12 +0100

    Add parsers for messages
    mostly method results

---
+++ unha2/parse/result.py
@@ -0,0 +1,111 @@
+from .. common import ts
+from .. model.base import RoomType, User
+
+def parse_login(result):
+    return {
+        'user_id': result['id'],
+        'token': result['token'],
+        'expires': ts(result['tokenExpires'])
+    }
+
+def parse_emojis(result):
+    return [{
+        'id': i['_id'],
+        'name': i['name'],
+        'aliases': i['aliases'],
+        'extension': i['extension'],
+        'last_update': ts(i['_updatedAt']['$date']),
+    } for i in result]
+
+def load_history(result):
+    return [{
+        'id': i['_id'],
+        'room_id': i['rid'],
+        'text': i['msg'],
+        'creation_time': ts(i['ts']),
+        'user': User(i['u']),
+        'update_time': ts(i['_updatedAt']),
+        'edition_time': ts(i['editedAt']) if i.get('editedAt') else None,
+        'edition_user': i.get('editedBy', ''),
+        'urls': i.get('urls', []),
+        'attachments': i.get('attachments', []),
+        'alias': i.get('alias', ''),
+        'avatar': i.get('avatar', ''),
+        'groupable': i.get('groupable', False),
+        'parse_urls': i.get('parseUrls', False)
+    } for i in result]
+
+def parse_user_roles(result):
+    return [{
+        'id': i['room-id'],
+        'user': User(i['u']),
+        'roles': i['roles'],
+        'role_id': i['_id']
+    } for i in result]
+
+def parse_subscriptions(result):
+    return {
+        'update': _parse_subscription_update(result['update']),
+        'remove': _parse_subscription_remove(result['remove'])
+    }
+
+def _parse_subscription_remove(result):
+    return [{
+        'room_id': i['_id'],
+        'deleted_at': ts(i['_deletedAt'])
+    } for i in result]
+
+def _parse_subscription_update(result):
+    return [{
+        'type': RoomType(i['t']),
+        'creation_time': ts(i['ts']) if i.get('ts') else None,
+        'last_seen_message': ts(i['ls']),
+        'name': i['name'],
+        'room_id': i['rid'],
+        'user': User(i['u']),
+        'open': i['open'],
+        'alert': i['alert'],
+        'roles': i.get('roles', []),
+        'unread': i['unread'],
+        'last_update': ts(i['_updatedAt']),
+        'sub_id': i['_id']
+    } for i in result]
+
+def parse_users(result):
+    return {
+        'total_number': result['total'],
+        'users': result['records']
+    }
+
+def parse_rooms(result):
+    return {
+        'update': _parse_rooms_update(result['update']),
+        'remove': _parse_rooms_remove(result['remove'])
+    }
+
+def _parse_rooms_update(rooms):
+    return [{
+        'id': room['_id'],
+        'type': RoomType(room['t']),
+        'name': room.get('name', ''),
+        'topic': room.get('topic', '')
+    } for room in rooms]
+
+def _parse_rooms_remove(rooms):
+    return [{'id': room['_id']} for room in rooms]
+
+def parse_permissions(result):
+    return [{
+        'id': i['_id'],
+        'roles': i['roles'],
+        'last_update': ts(i['_updatedAt']),
+        'meta': i['meta']
+    } for i in result]
+
+
+def parse_create_channel(result):
+    return {
+        'id': result['rid']
+    }
+
+parse_create_group = parse_create_channel
